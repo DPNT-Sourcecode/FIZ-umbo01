8b211d1f47592fdd9d03ef9676920d89604f54c4
sbx_user1059 <sbx_user1059@ip-10-128-149-18.eu-west-2.compute.internal>
2018-10-19 14:25:11 +0000

    Fri Oct 19 14:25:11 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -3,13 +3,17 @@ package befaster.solutions.FIZ;
 import befaster.runner.SolutionNotImplementedException;
 
 public class FizzBuzzSolution {
-
+//  - A number is considered to be "deluxe" if it is greater than 10 and all the digits are identical
     public String fizzBuzz(Integer number) {
-        boolean fizz=false, buzz = false;
+        boolean fizz=false, buzz = false, deluxe=false;
+        if(number>10 && hasAllDigitsIdentical(number))
+            deluxe=true;
         if(number%3 == 0 || has3(number))
             fizz=true;
         if(number%5 == 0 || has5(number))
             buzz=true;
+        if(fizz && buzz && deluxe)
+            return "fizz buzz deluxe";
         if(fizz && buzz)
             return "fizz buzz";
         if(fizz)
@@ -17,6 +21,19 @@ public class FizzBuzzSolution {
         if(buzz)
             return "buzz";
         return number.toString();     
+    }
+    public boolean hasAllDigitsIdentical(int number)
+    {
+        int digit=number%10;
+        number=number/10;
+        while(number>0)
+        {
+            if(number%10 != digit)
+                return false;
+            digit=number%10;
+            number=number/10;
+        }
+        return true;
     }
     public boolean has3(int number)
     {
@@ -28,6 +45,7 @@ public class FizzBuzzSolution {
         }
         return false;
     }
+ 
     public boolean has5(int number)
     {
         while(number>0)
